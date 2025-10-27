777c62003918a48b1bbc3a08e38b8b5cfb9e7b4b
Abdoulaye Cissokho <abdcsko@MacBook-Pro-de-Abdoulaye.local>
2025-10-27 17:22:04 +0100

    Ajout des fichiers du projet

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-5xl font-bold">Tailwind fonctionne ! 🎉</h1>
+      <p className="mt-4 text-lg text-gray-300">je suis prêt à coder mon portfolio 👌</p>
+    </div>
+  );
+}
+
+export default App;
