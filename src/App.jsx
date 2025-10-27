974bd5571bdeb8037770301fdf6d959e913c1261
Abdoulaye Cissokho <abdcsko@MacBook-Pro-de-Abdoulaye.local>
2025-10-27 17:33:33 +0100

    enfin ça marche

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import './App.css'
 function App() {
   return (
     <div className="text-center">
-      <h1 className="text-5xl font-bold">Tailwind fonctionne ! 🎉</h1>
-      <p className="mt-4 text-lg text-gray-300">je suis prêt à coder mon portfolio 👌</p>
+      <h1 className="text-5xl font-bold">Tailwind tu marche enfin ohlolololo ! 🎉</h1>
+      <p className="mt-4 text-lg text-gray-300">je suis prêt à coder mon portfolio et a vous cook ça 👌</p>
     </div>
   );
 }
