60ff022991857a1d8f2e8f6138640eca9b994ba2
Abdoulaye Cissokho <abdcsko@MacBook-Pro-de-Abdoulaye.local>
2025-10-28 14:54:05 +0100

    caca

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
 import './App.css'
-
 
 function App() {
   return (
+    <div>
+    <section className="">
     <div className="text-center">
-      <h1 className="text-5xl font-bold">Tailwind tu marche enfin ohlolololo ! 🎉</h1>
-      <p className="mt-4 text-lg text-gray-300">je suis prêt à coder mon portfolio et a vous cook ça 👌</p>
+      <h1 className="text-5xl font-bold">Bienvenue sur mon portfolio</h1>
+      <p className="mt-4 text-lg text-gray-300">je suis prêt à coder mon portfolio et a vous cook ça 👌 Bienvenue dans l'aventure <br />
+      je vais m'occuper de cette page en dernier</p>
     </div>
+    </section>
+    </div>
+
   );
 }
 
